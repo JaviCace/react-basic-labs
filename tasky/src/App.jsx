26288752c997a745e55bf7380df984a644b97ceb
JaviCace <149385126+JaviCace@users.noreply.github.com>
2026-09-16 14:05:48 +0200

    commit addtagsv5

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -5,6 +5,7 @@ import viteLogo from './assets/vite.svg'
 import './App.css'
 import Task from './components/Task.jsx'
 import AddTaskForm from './components/Form.jsx'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
    const [ taskState, setTaskState ] = useState({
@@ -14,6 +15,42 @@ function App() {
       { id:3 ,title: "Tidy up", deadline: "Today", priority:"High",done: false }
     ]
   });
+    const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
+  const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+  }
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+    
+    tasks.push(form);
+    setTaskState({tasks});
+  }
+
+
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
@@ -42,7 +79,8 @@ function App() {
 
     />
   ))}
-  <AddTaskForm />
+     <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+
   </div>
 );
 
